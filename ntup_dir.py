b366cc85f1618ab2178d0967528c5d9aa22c80c7
Dehua Zhu <zhud@phys.ethz.ch>
2018-05-18 10:36:05 +0200

    update on new directories for new samples

--- ntup_dir.py
+++ ntup_dir.py
@@ -1,5 +1,7 @@
 #Set the location where all ntuples are stored. Adapt the location to your path
 def getntupdir():
-    # ntup_dir = '/afs/cern.ch/user/d/dezhu/workspace/public/ntuples/' # location on lxplus
-    ntup_dir = '/Users/dehuazhu/Dropbox/PhD/5_Projects/analysis/180419_NTuples/0_ntuples/' # location on local machine
+    # ntup_dir = '/afs/cern.ch/user/d/dezhu/workspace/public/ntuples/201804_HNL' # location on lxplus
+    # ntup_dir = '/afs/cern.ch/user/d/dezhu/workspace/public/ntuples/201805_HNL' # location on lxplus
+    # ntup_dir = '/Users/dehuazhu/Dropbox/PhD/5_Projects/analysis/180419_NTuples/201804_HN3L/' # location on local machine
+    # ntup_dir = '/Users/dehuazhu/Dropbox/PhD/5_Projects/analysis/180419_NTuples/201805_HN3L/' # location on local machine
     return ntup_dir
