35d9166b8b179f980aa17cef3794f17902cfdfe3
Vinzenz <v.stampf@me.com>
2018-06-14 12:01:40 +0200

    6/14 updating nutple directory and HNLAna plot script

--- ntup_dir.py
+++ ntup_dir.py
@@ -3,6 +3,10 @@ def getntupdir():
     # ntup_dir = '/afs/cern.ch/user/d/dezhu/workspace/public/ntuples/201804_HNL/' # location on lxplus
     # ntup_dir = '/afs/cern.ch/user/d/dezhu/workspace/public/ntuples/201805_HNL/' # location on lxplus
     # ntup_dir = '/Users/dehuazhu/Dropbox/PhD/5_Projects/analysis/180419_NTuples/201804_HN3L/' # location on local machine
-    # ntup_dir = '/Users/dehuazhu/lxplus/public/ntuples/201806_HNLReco/' # location on local machine
-    ntup_dir = '/eos/user/d/dezhu/HNL/ntuple/20180608_HNLreco/ntuple/' # location on local machine
+    # ntup_dir = '/eos/user/d/dezhu/HNL/ntuple/20180608_HNLreco/ntuple/' # location on local machine
+    # ntup_dir = '/eos/user/v/vstampf/ntuple/' # eos 
+    # ntup_dir = '/afs/cern.ch/user/v/vstampf/CMSSW_8_0_30/src/CMGTools/HNL/cfg/cherry1/' # around 700k evts cherry1
+    # ntup_dir = '/eos/user/v/vstampf/ntupleN/' #full statistics modulo nonexistent vector<reco::Track> dSAmu
+    #ntup_dir = '/eos/user/v/vstampf/ntuplesfull/' # more evts, but not full and no idea what's inside
+    ntup_dir = '/afs/cern.ch/work/v/vstampf/ntuples/useable_full/' # latest (6/14) ntuples
     return ntup_dir
