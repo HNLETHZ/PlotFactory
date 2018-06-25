e90d66967665dc455afdd98ecfc016a26a0e8be1
Vinzenz <v.stampf@me.com>
2018-06-25 16:54:08 +0200

    updates and fixes

--- gentuple/ntup_dir.py
+++ gentuple/ntup_dir.py
@@ -2,13 +2,11 @@
 def getntupdir():
     # ntup_dir = '/afs/cern.ch/user/d/dezhu/workspace/public/ntuples/201804_HNL/' # location on lxplus
     # ntup_dir = '/afs/cern.ch/user/d/dezhu/workspace/public/ntuples/201805_HNL/' # location on lxplus
-    ntup_dir = '/eos/user/m/manzoni/HNL/gentuple_050318_v4/' # first version from riccardo
+    # ntup_dir = '/eos/user/m/manzoni/HNL/gentuple_050318_v4/' # first version from riccardo
     # ntup_dir = '/Users/dehuazhu/Dropbox/PhD/5_Projects/analysis/180419_NTuples/201804_HN3L/' # location on local machine
     # ntup_dir = '/eos/user/d/dezhu/HNL/ntuple/20180608_HNLreco/ntuple/' # global
-    # ntup_dir = '/eos/user/v/vstampf/ntuple/' # eos 
-    # ntup_dir = '/afs/cern.ch/user/v/vstampf/CMSSW_8_0_30/src/CMGTools/HNL/cfg/cherry1/' # around 700k evts cherry1
-    # ntup_dir = '/eos/user/v/vstampf/ntupleN/' #full statistics modulo nonexistent vector<reco::Track> dSAmu
-    # ntup_dir = '/eos/user/v/vstampf/ntuplesfull/' # more evts, but not full and no idea what's inside
-    # ntup_dir = '/afs/cern.ch/work/v/vstampf/ntuples/useable_full/' # latest (6/14) ntuples
-    # ntup_dir = '/afs/cern.ch/work/v/vstampf/ntuples/gen/' # latest (6/18) ntuples
+    # ntup_dir = '/afs/cern.ch/work/v/vstampf/public/ntuples/cherry1/' # around 700k evts cherry1
+    # ntup_dir = '/eos/user/v/vstampf/ntuples/reco_v613/' # latest (6/14) ntuples
+    ntup_dir = '/afs/cern.ch/work/v/vstampf/public/ntuples/gen_v1_refurbshd/' # latest (6/21) ntuples
+    # ntup_dir = '/afs/cern.ch/work/v/vstampf/public/ntuples/gen_v622/' # latest (6/22) new update
     return ntup_dir
