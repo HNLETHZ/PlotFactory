c370993ca6e22dcae8cdd7540535a80bfe88b870
dehuazhu <zhud@phys.ethz.ch>
2018-07-31 22:07:58 +0200

    updates to HNLAnalyzer

--- ntup_dir.py
+++ ntup_dir.py
@@ -1,7 +1,7 @@
 #Set the location where all ntuples are stored. Adapt the location to your path
 def getntupdir():
     # ntup_dir = '/afs/cern.ch/user/d/dezhu/workspace/public/ntuples/201804_HNL/' # location on lxplus
-    ntup_dir = '/afs/cern.ch/user/d/dezhu/workspace/public/ntuples/201805_HNL/' # location on lxplus
+    # ntup_dir = '/afs/cern.ch/user/d/dezhu/workspace/public/ntuples/201805_HNL/' # location on lxplus
     # ntup_dir = '/Users/dehuazhu/Dropbox/PhD/5_Projects/analysis/180419_NTuples/201804_HN3L/' # location on local machine
     # ntup_dir = '/eos/user/d/dezhu/HNL/ntuple/20180608_HNLreco/ntuple/' # location on local machine
     # ntup_dir = '/eos/user/v/vstampf/ntuple/' # eos 
@@ -9,6 +9,7 @@ def getntupdir():
     # ntup_dir = '/eos/user/v/vstampf/ntupleN/' #full statistics modulo nonexistent vector<reco::Track> dSAmu
     #ntup_dir = '/eos/user/v/vstampf/ntuplesfull/' # more evts, but not full and no idea what's inside
     # ntup_dir = '/afs/cern.ch/work/v/vstampf/ntuples/useable_full/' # latest (6/14) ntuples
-    # ntup_dir = '/eos/user/d/dezhu/HNL/ntuple/20180703_HNLreco/ntuples/'
+    # ntup_dir = '/eos/user/d/dezhu/HNL/ntuple/20180705_HNLreco_V2/ntuples/'
+    ntup_dir = '/eos/user/d/dezhu/HNL/ntuple/20180719_HNLreco/ntuples/'
 
     return ntup_dir
