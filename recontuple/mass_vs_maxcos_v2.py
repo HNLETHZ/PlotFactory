fa8786e4c628d5fb38305619ef648f14f2974148
Vinzenz <v.stampf@me.com>
2018-08-07 18:31:11 +0200

    adding ctau check and

--- recontuple/mass_vs_maxcos_v2.py
+++ recontuple/mass_vs_maxcos_v2.py
@@ -19,55 +19,149 @@ pf.setpfstyle()
 ####################################################################################################
 ####################################################################################################
 ####################################################################################################
-b_masses = np.arange(0.,11,0.1)
+b_m = np.arange(0.,11,0.1)
 ####################################################################################################
 ####################################################################################################
 ####################################################################################################
-h_masses_eff_denom = rt.TH1F('masses_eff_denom','masses_eff_denom',len(b_masses)-1,b_masses)
-h_masses_eff_numer = rt.TH1F('masses_eff_numer','masses_eff_numer',len(b_masses)-1,b_masses)
-h_masses_pur_denom = rt.TH1F('masses_pur_denom','masses_pur_denom',len(b_masses)-1,b_masses)
-h_masses_pur_numer = rt.TH1F('masses_pur_numer','masses_pur_numer',len(b_masses)-1,b_masses)
+h_m_maxcos_eff_d = rt.TH1F('m_maxcos_eff_d','m_maxcos_eff_d',len(b_m)-1,b_m)
+h_m_maxcos_eff_n = rt.TH1F('m_maxcos_eff_n','m_maxcos_eff_n',len(b_m)-1,b_m)
+####################################################################################################
+h_m_maxcos_pur_d = rt.TH1F('m_maxcos_pur_d','m_maxcos_pur_d',len(b_m)-1,b_m)
+h_m_maxcos_pur_n = rt.TH1F('m_maxcos_pur_n','m_maxcos_pur_n',len(b_m)-1,b_m)
+####################################################################################################
+####################################################################################################
+h_m_maxdxy_eff_d = rt.TH1F('m_maxdxy_eff_d','m_maxdxy_eff_d',len(b_m)-1,b_m)
+h_m_maxdxy_eff_n = rt.TH1F('m_maxdxy_eff_n','m_maxdxy_eff_n',len(b_m)-1,b_m)
+####################################################################################################
+h_m_maxdxy_pur_d = rt.TH1F('m_maxdxy_pur_d','m_maxdxy_pur_d',len(b_m)-1,b_m)
+h_m_maxdxy_pur_n = rt.TH1F('m_maxdxy_pur_n','m_maxdxy_pur_n',len(b_m)-1,b_m)
+####################################################################################################
+####################################################################################################
+h_m_mindr_eff_d = rt.TH1F('m_mindr_eff_d','m_mindr_eff_d',len(b_m)-1,b_m)
+h_m_mindr_eff_n = rt.TH1F('m_mindr_eff_n','m_mindr_eff_n',len(b_m)-1,b_m)
+####################################################################################################
+h_m_mindr_pur_d = rt.TH1F('m_mindr_pur_d','m_mindr_pur_d',len(b_m)-1,b_m)
+h_m_mindr_pur_n = rt.TH1F('m_mindr_pur_n','m_mindr_pur_n',len(b_m)-1,b_m)
+####################################################################################################
+####################################################################################################
+h_m_maxpt_eff_d = rt.TH1F('m_maxpt_eff_d','m_maxpt_eff_d',len(b_m)-1,b_m)
+h_m_maxpt_eff_n = rt.TH1F('m_maxpt_eff_n','m_maxpt_eff_n',len(b_m)-1,b_m)
+####################################################################################################
+h_m_maxpt_pur_d = rt.TH1F('m_maxpt_pur_d','m_maxpt_pur_d',len(b_m)-1,b_m)
+h_m_maxpt_pur_n = rt.TH1F('m_maxpt_pur_n','m_maxpt_pur_n',len(b_m)-1,b_m)
+####################################################################################################
+####################################################################################################
+h_m_minchi2_eff_d = rt.TH1F('m_minchi2_eff_d','m_minchi2_eff_d',len(b_m)-1,b_m)
+h_m_minchi2_eff_n = rt.TH1F('m_minchi2_eff_n','m_minchi2_eff_n',len(b_m)-1,b_m)
+####################################################################################################
+h_m_minchi2_pur_d = rt.TH1F('m_minchi2_pur_d','m_minchi2_pur_d',len(b_m)-1,b_m)
+h_m_minchi2_pur_n = rt.TH1F('m_minchi2_pur_n','m_minchi2_pur_n',len(b_m)-1,b_m)
 ####################################################################################################
 ####################################################################################################
 ####################################################################################################
-t.Draw('hnl_hn_MASS >> masses_eff_numer','flag_IsThereTHEDimuon == 1 & flag_matchedHNLMaxCosBPA == 1')
-t.Draw('hnl_hn_MASS >> masses_eff_denom','flag_IsThereTHEDimuon == 1')
+t.Draw('hnl_hn_MASS >> m_maxcos_eff_n','flag_IsThereTHEDimuon == 1 & flag_matchedHNLMaxCosBPA == 1')
+t.Draw('hnl_hn_MASS >> m_maxcos_eff_d','flag_IsThereTHEDimuon == 1')
 ####################################################################################################
-t.Draw('hnl_hn_MASS >> masses_pur_numer','flag_matchedHNLMaxCosBPA == 1')
-t.Draw('hnl_hn_MASS >> masses_pur_denom','flag_matchedHNLMaxCosBPA == 1 | flag_matchedHNLMaxCosBPA == 0')
-####################################################################################################
-####################################################################################################
-####################################################################################################
-h_masses_pur_numer.Divide(h_masses_pur_denom)
-h_masses_eff_numer.Divide(h_masses_eff_denom)
+t.Draw('hnl_hn_MASS >> m_maxcos_pur_n','flag_matchedHNLMaxCosBPA == 1')
+t.Draw('hnl_hn_MASS >> m_maxcos_pur_d','flag_matchedHNLMaxCosBPA == 1 | flag_matchedHNLMaxCosBPA == 0')
 ####################################################################################################
 ####################################################################################################
-hlist = [h_masses_pur_numer,h_masses_eff_numer]
+t.Draw('hnl_hn_MASS >> m_maxdxy_eff_n','flag_IsThereTHEDimuon == 1 & flag_matchedHNLDxy == 1')
+t.Draw('hnl_hn_MASS >> m_maxdxy_eff_d','flag_IsThereTHEDimuon == 1')
+####################################################################################################
+t.Draw('hnl_hn_MASS >> m_maxdxy_pur_n','flag_matchedHNLDxy == 1')
+t.Draw('hnl_hn_MASS >> m_maxdxy_pur_d','flag_matchedHNLDxy == 1 | flag_matchedHNLDxy == 0')
 ####################################################################################################
 ####################################################################################################
-for h in hlist:
+t.Draw('hnl_hn_MASS >> m_mindr_eff_n','flag_IsThereTHEDimuon == 1 & flag_matchedHNLMinDr12 == 1')
+t.Draw('hnl_hn_MASS >> m_mindr_eff_d','flag_IsThereTHEDimuon == 1')
+####################################################################################################
+t.Draw('hnl_hn_MASS >> m_mindr_pur_n','flag_matchedHNLMinDr12 == 1')
+t.Draw('hnl_hn_MASS >> m_mindr_pur_d','flag_matchedHNLMinDr12 == 1 | flag_matchedHNLMinDr12 == 0')
+####################################################################################################
+####################################################################################################
+t.Draw('hnl_hn_MASS >> m_maxpt_eff_n','flag_IsThereTHEDimuon == 1 & flag_matchedHNLMaxPt == 1')
+t.Draw('hnl_hn_MASS >> m_maxpt_eff_d','flag_IsThereTHEDimuon == 1')
+####################################################################################################
+t.Draw('hnl_hn_MASS >> m_maxpt_pur_n','flag_matchedHNLMaxPt == 1')
+t.Draw('hnl_hn_MASS >> m_maxpt_pur_d','flag_matchedHNLMaxPt == 1 | flag_matchedHNLMaxPt == 0')
+####################################################################################################
+####################################################################################################
+t.Draw('hnl_hn_MASS >> m_minchi2_eff_n','flag_IsThereTHEDimuon == 1 & flag_matchedHNLChi2 == 1')
+t.Draw('hnl_hn_MASS >> m_minchi2_eff_d','flag_IsThereTHEDimuon == 1')
+####################################################################################################
+t.Draw('hnl_hn_MASS >> m_minchi2_pur_n','flag_matchedHNLChi2 == 1')
+t.Draw('hnl_hn_MASS >> m_minchi2_pur_d','flag_matchedHNLChi2 == 1 | flag_matchedHNLChi2 == 0')
+####################################################################################################
+####################################################################################################
+####################################################################################################
+h_m_maxcos_pur_n.Divide(h_m_maxcos_pur_d)
+h_m_maxcos_eff_n.Divide(h_m_maxcos_eff_d)
+h_m_maxcos_pur_n.SetMarkerColor(rt.kBlue+2)
+h_m_maxcos_eff_n.SetMarkerColor(rt.kBlue+2)
+h_m_maxcos_eff_n.SetTitle('MaxCosBPA')
+h_m_maxcos_pur_n.SetTitle('MaxCosBPA')
+####################################################################################################
+h_m_maxdxy_pur_n.Divide(h_m_maxdxy_pur_d)
+h_m_maxdxy_eff_n.Divide(h_m_maxdxy_eff_d)
+h_m_maxdxy_pur_n.SetMarkerColor(rt.kYellow+2)
+h_m_maxdxy_eff_n.SetMarkerColor(rt.kYellow+2)
+h_m_maxdxy_pur_n.SetTitle('Max D_{xy}') 
+h_m_maxdxy_eff_n.SetTitle('Max D_{xy}')
+####################################################################################################
+h_m_mindr_pur_n.Divide(h_m_mindr_pur_d)
+h_m_mindr_eff_n.Divide(h_m_mindr_eff_d)
+h_m_mindr_pur_n.SetMarkerColor(rt.kCyan+2)
+h_m_mindr_eff_n.SetMarkerColor(rt.kCyan+2)
+h_m_mindr_pur_n.SetTitle('Min #Deltar(l_{1},l_{2})') 
+h_m_mindr_eff_n.SetTitle('Min #Deltar(l_{1},l_{2})')
+####################################################################################################
+h_m_maxpt_pur_n.Divide(h_m_maxpt_pur_d)
+h_m_maxpt_eff_n.Divide(h_m_maxpt_eff_d)
+h_m_maxpt_pur_n.SetMarkerColor(rt.kGreen+2)
+h_m_maxpt_eff_n.SetMarkerColor(rt.kGreen+2)
+h_m_maxpt_pur_n.SetTitle('Max p_{T}')
+h_m_maxpt_eff_n.SetTitle('Max p_{T}')
+####################################################################################################
+h_m_minchi2_pur_n.Divide(h_m_minchi2_pur_d)
+h_m_minchi2_eff_n.Divide(h_m_minchi2_eff_d)
+h_m_minchi2_pur_n.SetMarkerColor(rt.kRed+2)
+h_m_minchi2_eff_n.SetMarkerColor(rt.kRed+2)
+h_m_minchi2_pur_n.SetTitle('Min #chi^{2}')
+h_m_minchi2_eff_n.SetTitle('Min #chi^{2}')
+####################################################################################################
+####################################################################################################
+lst_pur = [h_m_maxpt_pur_n,h_m_maxdxy_pur_n,h_m_maxcos_pur_n,h_m_mindr_pur_n,h_m_minchi2_pur_n]
+lst_eff = [h_m_maxpt_eff_n,h_m_maxdxy_eff_n,h_m_maxcos_eff_n,h_m_mindr_eff_n,h_m_minchi2_eff_n]
+####################################################################################################
+####################################################################################################
+for h in lst_pur+lst_eff:
     h.GetXaxis().SetTitle('HNL Mass [GeV]')
-    h.SetMarkerColor(rt.kBlue+2)
-    h.SetTitle('Selection: MaxCosBPA')
-h_masses_eff_numer.SetAxisRange(0.95,1.005,'Y')
-h_masses_eff_numer.GetYaxis().SetTitle('Efficiency')
-h_masses_pur_numer.GetYaxis().SetTitle('Purity')
+    h.SetMarkerSize(0.3)
+for h in lst_eff:
+    h.SetAxisRange(0.92,1.005,'Y')
+    h.GetYaxis().SetTitle('Efficiency')
+for h in lst_pur:
+    h.GetYaxis().SetTitle('Purity')
+    h.SetAxisRange(0.3,0.905,'Y')
 ####################################################################################################
 ####################################################################################################
 ####################################################################################################
-c_masses_eff = rt.TCanvas('masses_eff','masses_eff')
-c_masses_pur = rt.TCanvas('masses_pur','masses_pur')
+c_m_eff = rt.TCanvas('m_eff','m_eff')
+c_m_pur = rt.TCanvas('m_pur','m_pur')
 ####################################################################################################
 ####################################################################################################
-clist = [c_masses_pur,c_masses_eff]
+clist = [c_m_pur,c_m_eff]
 ####################################################################################################
 ####################################################################################################
-c_masses_eff .cd()
-h_masses_eff_numer.Draw('ep')
+c_m_eff.cd()
+for h in lst_eff: h.Draw('epsame')
+c_m_eff.BuildLegend()
 ####################################################################################################
 ####################################################################################################
-c_masses_pur.cd()
-h_masses_pur_numer.Draw('ep')
+c_m_pur.cd()
+for h in lst_pur: h.Draw('epsame')
+c_m_pur.BuildLegend()
 ####################################################################################################
 ####################################################################################################
 for c in clist:
